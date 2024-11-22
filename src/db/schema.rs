346053b7ad514a5d348a20df3cff638156628d20
Yuki.N <Syukkic@gmail.com>
2024-11-22 17:00:02 +0800

    Updated db schema

--- src/db/schema.rs
+++ src/db/schema.rs
@@ -3,7 +3,8 @@
 diesel::table! {
     users (chat_id) {
         chat_id -> BigInt,
+        user_id -> BigInt,
+        username -> Text,
         reminder_time -> Text,
-        last_reminder_time -> Nullable<Text>,
     }
 }
