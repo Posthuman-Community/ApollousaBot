cb9604957ca2cc259b5e046b47e6528908d4d784
Yuki.N <Syukkic@gmail.com>
2024-11-26 15:20:55 +0800

    Adjusted user reminder time calculations based on UTC offset

--- src/scheduler/reminder.rs
+++ src/scheduler/reminder.rs
@@ -1,7 +1,8 @@
 use std::sync::Arc;
 
 use crate::db::action as operator;
-use chrono::Local;
+use crate::utils::TimezoneOffest;
+use chrono::{FixedOffset, Utc};
 use teloxide::adaptors::DefaultParseMode;
 use teloxide::prelude::*;
 use teloxide::types::ChatId;
@@ -13,18 +14,30 @@ type Bot = DefaultParseMode<teloxide::Bot>;
 
 pub async fn schedule_reminders(bot: &Arc<Bot>) {
     let conn = &mut establish_connection();
-    let current_time = Local::now().time().format("%H:%M").to_string();
-
-    let users_to_remind = operator::get_user_reminders(conn, &current_time);
+    let users_to_remind = operator::get_user_reminders(conn);
 
     for user in users_to_remind {
-        println!("{}", user.reminder_time);
-        let _user_id = UserId(u64::try_from(user.user_id).unwrap());
-        let _username = user.username;
-        let mentioned_user = html::user_mention(_user_id, &_username);
-        let notification = format!("{mentioned_user}，记得起身活动活动，松松背拉拉筋。");
-        bot.send_message(ChatId(user.chat_id), notification)
-            .await
-            .unwrap();
+        if let Some(tzoffset) = user.tz_offset.as_ref() {
+            let user_timezone: TimezoneOffest = tzoffset.parse().unwrap();
+
+            let duration_secs = user_timezone.to_duration();
+            let user_utc_time = Utc::now()
+                .with_timezone(&FixedOffset::east_opt(duration_secs).unwrap())
+                .format("%H:%M")
+                .to_string();
+
+            // println!("User UTC time: {}", user_utc_time);
+
+            if user_utc_time == user.reminder_time {
+                // println!("{}: {}", user.reminder_time, user.username);
+                let _user_id = UserId(u64::try_from(user.user_id).unwrap());
+                let _username = user.username;
+                let mentioned_user = html::user_mention(_user_id, &_username);
+                let notification = format!("{mentioned_user}，记得起身活动活动，松松背拉拉筋。");
+                bot.send_message(ChatId(user.chat_id), notification)
+                    .await
+                    .unwrap();
+            }
+        }
     }
 }
