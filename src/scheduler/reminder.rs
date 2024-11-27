1bec737eee6825d73b8f30711658d9e5b975200d
Yuki.N <Syukkic@gmail.com>
2024-11-27 16:36:19 +0800

    Updated reminder text to a random quote

--- src/scheduler/reminder.rs
+++ src/scheduler/reminder.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
 use crate::db::action as operator;
-use crate::utils::TimezoneOffest;
+use crate::utils::{get_random_quote, TimezoneOffest};
 use chrono::{FixedOffset, Utc};
 use teloxide::adaptors::DefaultParseMode;
 use teloxide::prelude::*;
@@ -16,6 +16,10 @@ pub async fn schedule_reminders(bot: &Arc<Bot>) {
     let conn = &mut establish_connection();
     let users_to_remind = operator::get_user_reminders(conn);
 
+    let all_quotes = operator::get_quotes(conn);
+    let random_quote = get_random_quote(&all_quotes);
+    let selected_quote = random_quote.unwrap_or("记得起身活动活动，松松背拉拉筋。");
+
     for user in users_to_remind {
         if let Some(tzoffset) = user.tz_offset.as_ref() {
             let user_timezone: TimezoneOffest = tzoffset.parse().unwrap();
@@ -26,14 +30,11 @@ pub async fn schedule_reminders(bot: &Arc<Bot>) {
                 .format("%H:%M")
                 .to_string();
 
-            // println!("User UTC time: {}", user_utc_time);
-
             if user_utc_time == user.reminder_time {
-                // println!("{}: {}", user.reminder_time, user.username);
                 let _user_id = UserId(u64::try_from(user.user_id).unwrap());
                 let _username = user.username;
                 let mentioned_user = html::user_mention(_user_id, &_username);
-                let notification = format!("{mentioned_user}，记得起身活动活动，松松背拉拉筋。");
+                let notification = format!("{mentioned_user} {selected_quote}");
                 bot.send_message(ChatId(user.chat_id), notification)
                     .await
                     .unwrap();
